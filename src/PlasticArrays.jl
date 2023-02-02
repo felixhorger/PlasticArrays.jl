b619adcfe695c78f52e5ba1a04422cb749575856
Felix Horger <felix.horger@kcl.ac.uk>
2023-02-02 12:42:14 +0000

    Improved code structure

--- src/PlasticArrays.jl
+++ src/PlasticArrays.jl
@@ -9,12 +9,13 @@ module PlasticArrays
 		PlasticArray(nbytes::Integer) = new(nbytes, Vector{UInt8}(undef, nbytes))
 	end
 
-	function mould(a::PlasticArray, dtype::Type, shape::Integer...)
+	function mould(a::PlasticArray, dtype::Type, shape::Tuple{Vararg{Integer}})
 		@assert prod(shape) * sizeof(dtype) ≤ a.nbytes
 		GC.@preserve a begin
 			arr = unsafe_wrap(Array{dtype, length(shape)}, convert(Ptr{dtype}, pointer(a.mem)), shape; own=false)
 			return arr
 		end
 	end
+	mould(a::PlasticArray, dtype::Type, shape::Integer...) = mould(a, dtype, shape)
 
 end
